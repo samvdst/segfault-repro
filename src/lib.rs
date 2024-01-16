e45afa89d9719ce3bec32461b97512211b41657e
Samuel Van der Stappen <sam@vd.st>
2024-01-16 14:32:16 +0100

    even smaller

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,2 @@
-#![allow(unused_imports)]
-use napi::Result;
-use sqlx::MssqlPool;
-use sqlx_oldapi::{types::chrono::Utc, SqlitePool};
-use std::{fmt::Debug, sync::Arc};
-use tokio::{sync::Mutex, task::block_in_place};
-use uuid::Uuid;
 
-#[macro_use]
-extern crate napi_derive;
-extern crate sqlx as sqlx_oldapi;
+
